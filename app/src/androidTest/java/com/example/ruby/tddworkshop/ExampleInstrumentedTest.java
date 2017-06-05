df3b9dc7da066f4154fe31c0b5f12903b7053cdd
Ruby <ruby@200apps.co>
2017-06-05 20:46:36 +0300

    integration test red iteration

--- app/src/androidTest/java/com/example/ruby/tddworkshop/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/ruby/tddworkshop/ExampleInstrumentedTest.java
@@ -1,24 +1,30 @@
 package com.example.ruby.tddworkshop;
 
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
 /**
  * Instrumentation test, which will execute on an Android device.
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-@RunWith(AndroidJUnit4.class) public class ExampleInstrumentedTest {
-  @Test public void useAppContext() throws Exception {
-    // Context of the app under test.
-    Context appContext = InstrumentationRegistry.getTargetContext();
+@RunWith(AndroidJUnit4.class) @LargeTest public class ExampleInstrumentedTest {
 
-    assertEquals("com.example.ruby.tddworkshop", appContext.getPackageName());
+  @Rule public ActivityTestRule<MainActivity> mActivityRule =
+      new ActivityTestRule<>(MainActivity.class);
+
+  @Test public void flow1() {
+
+    // Check that No groups msg is displayed
+    onView(withId(R.id.tv_no_groups)).check(matches(isDisplayed()));
   }
 }
---
+++ app/src/androidTest/java/com/example/ruby/tddworkshop/UseCaseTest.java
@@ -0,0 +1,8 @@
+package com.example.ruby.tddworkshop;
+
+/**
+ * Created by Ruby on 6/5/2017.
+ */
+
+public class UseCaseTest {
+}
